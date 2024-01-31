05f1fa6332de7919ae8f08b553bec4ba15783b88
Adithya Karthik <karthiad@shernet.sheridancollege.ca>
2024-01-31 10:04:06 -0500

    Week4-Task1 Complete, added a plane and line renderer.

---
+++ Assets/Week 4/Scripts/Plane.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Plane : MonoBehaviour
+{
+    public List<Vector2> points;
+    public float newPointThreshold = 0.2f;
+    Vector2 lastPosition;
+    LineRenderer lineRenderer;
+
+    private void OnMouseDown()
+    {
+       points = new List<Vector2>();
+        Vector2 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        points.Add(currentPosition);
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition(0, transform.position);
+
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector2 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if(Vector2.Distance(currentPosition, lastPosition) > newPointThreshold)
+        {
+            points.Add(currentPosition);
+            lineRenderer.positionCount++;
+            lineRenderer.SetPosition(lineRenderer.positionCount - 1, currentPosition);
+
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition(0,transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
