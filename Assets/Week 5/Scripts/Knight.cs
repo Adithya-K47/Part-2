fcdb2a61ce8313ea94f8366c711e15a063506767
Adithya Karthik <karthiad@shernet.sheridancollege.ca>
2024-02-07 11:58:58 -0500

    added a health bar and campfire

--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,15 +13,19 @@ public class Knight : MonoBehaviour
     bool clickingOnself = false;
     public float health;
     float maxHealth = 5;
+    bool isDead;
+    public HealthBar healthbar;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         health = maxHealth;
+        isDead = false;
     }
 
     private void FixedUpdate()
     {
+        if (isDead) return;
         movement = destination - (Vector2)transform.position;
         if(movement.magnitude < 0.1) 
         {
@@ -32,7 +37,8 @@ public class Knight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0) && !clickingOnself)
+        if (isDead) return;
+        if (Input.GetMouseButtonDown(0) && !clickingOnself)
         {
             destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
@@ -41,25 +47,28 @@ public class Knight : MonoBehaviour
 
     private void OnMouseDown()
     {
+        if (isDead) return;
         clickingOnself = true;
-        TakeDamage(1);
+        gameObject.SendMessage("TakeDamage", 1);
+       
     }
 
     private void OnMouseUp()
     {
         clickingOnself = false;
     }
-    void TakeDamage(float damage)
+    public void TakeDamage(float damage)
     {
         health -= damage;
         health = Mathf.Clamp(health, 0, maxHealth);
         if(health <= 0)
         {
-            //die?
+            isDead = true;
             animator.SetTrigger("Death");
         }
         else
         {
+            isDead = false;
             animator.SetTrigger("TakeDamage");
         }
     }
