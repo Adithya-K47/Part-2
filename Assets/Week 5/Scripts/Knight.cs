2c09aed6410042ab7b49a427a3edf8f0965db91e
Adithya Karthik <karthiad@shernet.sheridancollege.ca>
2024-02-07 10:49:28 -0500

    Added hit animation and death animation for knight.

---
+++ Assets/Week 5/Scripts/Knight.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator animator;
+    Vector2 destination;
+    Vector2 movement;
+    public float speed = 3;
+    bool clickingOnself = false;
+    public float health;
+    float maxHealth = 5;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        health = maxHealth;
+    }
+
+    private void FixedUpdate()
+    {
+        movement = destination - (Vector2)transform.position;
+        if(movement.magnitude < 0.1) 
+        {
+            movement = Vector2.zero;
+        }
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && !clickingOnself)
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        animator.SetFloat("Movement", movement.magnitude);
+    }
+
+    private void OnMouseDown()
+    {
+        clickingOnself = true;
+        TakeDamage(1);
+    }
+
+    private void OnMouseUp()
+    {
+        clickingOnself = false;
+    }
+    void TakeDamage(float damage)
+    {
+        health -= damage;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        if(health <= 0)
+        {
+            //die?
+            animator.SetTrigger("Death");
+        }
+        else
+        {
+            animator.SetTrigger("TakeDamage");
+        }
+    }
+}
