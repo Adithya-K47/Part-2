77cf002d598fbb639b5fb8537cc5dc7b9448b64e
Adithya-K47 <karthiad@sheridancollege.ca>
2024-02-15 00:49:14 -0500

    Week task 4a and 4b

--- Assets/Week 6/Scripts/Resolution.cs
+++ Assets/Week 6/Scripts/Resolution.cs
@@ -9,12 +9,12 @@ public class Resolution : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Screen.SetResolution(1280, 720, FullScreenMode.Windowed);
+        Screen.SetResolution(1600, 900, FullScreenMode.Windowed);
     }
 
     public void Res1()
     {
-        Screen.SetResolution(1280, 720, FullScreenMode.Windowed);
+        Screen.SetResolution(1600, 900, FullScreenMode.Windowed);
     }
 
     public void Res2()
