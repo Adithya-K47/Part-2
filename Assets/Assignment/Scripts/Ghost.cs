dab6b8160dc15f29ce17cf721a41367c59d55461
Adithya-K47 <karthiad@sheridancollege.ca>
2024-02-21 21:42:41 -0500

    Player movement working

--- Assets/Assignment/Scripts/Ghost.cs
+++ Assets/Assignment/Scripts/Ghost.cs
@@ -1,5 +1,4 @@
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,21 +13,19 @@ public class Ghost : MonoBehaviour
     public float speed = 3;
     public float hp;
     float maxHP = 5;
-    public bool isDown;
     public HealthBar hb;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        isDown = false;
         hp = maxHP;
-        
+
     }
     private void FixedUpdate()
     {
-        if (isDown) return;
         mov = dest - (Vector2)transform.position;
+        Debug.Log(Equals(dest, mov));
         if (mov.magnitude < 0.1)
         {
             mov = Vector2.zero;
@@ -39,29 +36,16 @@ public class Ghost : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isDown) return;
-        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        if (Input.GetMouseButtonDown(0))
         {
             dest = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
-        animator.SetFloat("Movement", mov.magnitude);
         gameObject.SendMessage("HBval", hp, SendMessageOptions.DontRequireReceiver);
     }
 
 
     public void TakeDamage(float damage)
     {
-        hp -= damage;
-        hp = Mathf.Clamp(hp, 0, maxHP);
-        if (hp <= 0)
-        {
-            isDown = true;
-            animator.SetTrigger("Death");
-        }
-        else
-        {
-            isDown = false;
-            animator.SetTrigger("TakeDamage");
-        }
+       
     }
 }
