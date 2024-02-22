73a26e20508515d7fe648b936f82d1b4ef96d858
Adithya-K47 <karthiad@sheridancollege.ca>
2024-02-21 22:43:48 -0500

    Created tileset, added slime, added slimerspawner, slime script.

--- Assets/Assignment/Scripts/Ghost.cs
+++ Assets/Assignment/Scripts/Ghost.cs
@@ -7,12 +7,13 @@ using UnityEngine.EventSystems;
 public class Ghost : MonoBehaviour
 {
     Rigidbody2D rb;
-    Animator animator;
+    public Animator animator;
     Vector2 dest;
     Vector2 mov;
     public float speed = 3;
     public float hp;
     float maxHP = 5;
+    public bool Down;
     public HealthBar hb;
     
     // Start is called before the first frame update
@@ -20,12 +21,12 @@ public class Ghost : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         hp = maxHP;
+        Down = false;
 
     }
     private void FixedUpdate()
     {
         mov = dest - (Vector2)transform.position;
-        Debug.Log(Equals(dest, mov));
         if (mov.magnitude < 0.1)
         {
             mov = Vector2.zero;
@@ -41,11 +42,23 @@ public class Ghost : MonoBehaviour
             dest = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
         gameObject.SendMessage("HBval", hp, SendMessageOptions.DontRequireReceiver);
+        animator.SetFloat("Movement", mov.magnitude);
     }
 
 
     public void TakeDamage(float damage)
     {
-       
+        hp -= damage;
+        hp = Mathf.Clamp(hp, 0, maxHP);
+        if (hp <= 0)
+        {
+            Down = true;
+            animator.SetTrigger("Death");
+        }
+        else
+        {
+            Down = false;
+            animator.SetTrigger("TakeDamage");
+        }
     }
 }
